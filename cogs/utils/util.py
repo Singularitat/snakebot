bf5876c9451e55c6b2a8c246f3ac03a54301f62e
singularitat <singularbotnet@gmail.com>
2021-01-10 19:47:20 +1300

    Updated Utils

--- cogs/utils/util.py
+++ cogs/utils/util.py
@@ -1,6 +1,4 @@
-import aiohttp
 import re
-from bs4 import BeautifulSoup
 import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -12,22 +10,6 @@ def get_matching_emote(guild, emote):
         if emote.name == emote_name:
             matching_emote = emote
     return matching_emote
-
-
-async def stockgrab(url):
-    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url) as page:
-            soup = BeautifulSoup(await page.text(),  'html.parser')
-    stockdata = []
-    table = soup.find('table', attrs={'class': 'W(100%)'})
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        stockdata.append([ele for ele in cols if ele])
-    return stockdata
 
 
 def _stringify_time_unit(value: int, unit: str) -> str:
