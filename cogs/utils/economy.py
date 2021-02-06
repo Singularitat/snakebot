be963500d9920b7094941f4e0e8eddb696ed2610
singularitat <singularbotnet@gmail.com>
2021-02-06 21:56:58 +1300

    bs4 to lxml.html

--- cogs/utils/economy.py
+++ cogs/utils/economy.py
@@ -1,18 +1,21 @@
-from bs4 import BeautifulSoup
+import lxml.html
 import aiohttp
 
 
 async def stockgrab(url):
-    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
+    """Grabs some information abouts stocks from yahoo stocks."""
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
+    }
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as page:
-            soup = BeautifulSoup(await page.text(),  'html.parser')
+            soup = lxml.html.fromstring(await page.text())
     stockdata = []
-    table = soup.find('table', attrs={'class': 'W(100%)'})
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        stockdata.append([ele for ele in cols if ele])
+    for table in soup.xpath('.//table[@class="W(100%)"]'):
+        table_body = table.find("tbody")
+        rows = table_body.findall("tr")
+        for row in rows:
+            cols = row.findall("td")
+            cols = [ele.text_content().strip() for ele in cols]
+            stockdata.append([ele for ele in cols if ele])
     return stockdata
