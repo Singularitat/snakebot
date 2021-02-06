4d410b5b3791ef7f05a5ee15e662666d2ceb5d73
singularitat <singularbotnet@gmail.com>
2021-02-06 21:16:03 +1300

    Fixed intents, cleaned up formatting, switched token storage

--- bot.py
+++ bot.py
@@ -1,33 +1,29 @@
 import discord
 from discord.ext import commands
-import json
-from os import listdir
-from os.path import isfile, join
-
+import os
+import config
 
 # Connects to discord, Sets command prefix, Removes default help command
 # Sets discord intents to enable certain gateway features that are necessary
 
-intents = discord.Intents(guilds=True, members=True, bans=True, emojis=True, voice_states=True, messages=True, reactions=True)
-bot = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix=".", help_command=None)
-
-# Gets discord TOKEN
-
-with open('json/data.txt') as json_file:
-    data = json.load(json_file)
-    TOKEN = (data['token'])
-
-# Runs all the the cogs in /cogs as extensions
-
-cogs_dir = "cogs"
+bot = commands.Bot(
+    intents=discord.Intents.all(),
+    command_prefix=commands.when_mentioned_or("."),
+    help_command=None,
+    case_insensitive=True,
+    owner_id=225708387558490112,
+    activity=discord.Game(name="Tax Evasion Simulator"),
+)
 
 if __name__ == "__main__":
-    for extension in [f.replace('.py', '') for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]:
+    for extension in [
+        f.replace(".py", "")
+        for f in os.listdir("cogs")
+        if os.path.isfile(os.path.join("cogs", f))
+    ]:
         try:
-            bot.load_extension(cogs_dir + "." + extension)
+            bot.load_extension(f"cogs.{extension}")
         except Exception as e:
-            print(f'Failed to load extension {extension}.')
-            print(f'{e} \n')
+            print(f"Failed to load extension {extension}.\n{e} \n")
 
-bot.run(TOKEN)
+bot.run(config.token)
