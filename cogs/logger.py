5c21040669915b62abc1ac251083ea31acce4889
singularitat <singularbotnet@gmail.com>
2021-02-15 15:52:34 +1300

    Fix logging cog

--- cogs/logger.py
+++ cogs/logger.py
@@ -2,26 +2,38 @@ from discord.ext import commands
 import logging
 import requests
 import config
+import json
+
+session = requests.Session()
 
 
 class RequestsHandler(logging.Handler):
     def emit(self, record):
-        log_entry = self.format(record)
-        return None
-        return requests.get(
-            f"https://snakebotdashboard.qw.ms/api/log/new?token={config.loggingtoken}&error={log_entry}"
-        ).content
+        try:
+            log_entry = json.loads(self.format(record))
+        except json.decoder.JSONDecodeError:
+            log_entry = {
+                "message": record.msg,
+                "level": record.levelname,
+                "name": record.name,
+                "line": record.lineno,
+                "time": record.asctime,
+            }
+        return session.post(
+            f"https://snakebotdashboard.qw.ms/api/log/new?token={config.loggingtoken}",
+            json=log_entry,
+        )
 
 
 class RemoveNoise(logging.Filter):
     def __init__(self):
         super().__init__(name="discord.state")
 
-    # @staticmethod
-    # def filter(self, record):
-    #     if record.levelname == "WARNING" and "referencing an unknown" in record.msg:
-    #         return False
-    #     return True
+    @staticmethod
+    def filter(self, record):
+        if record.levelname == "WARNING" and "referencing an unknown" in record.msg:
+            return False
+        return True
 
 
 logging.getLogger("discord")
@@ -33,7 +45,7 @@ log.setLevel(logging.INFO)
 custom_handler = RequestsHandler()
 custom_handler.setFormatter(
     logging.Formatter(
-        "{'@message': %(message)s, '@level': %(levelname)s, '@name': %(name)s, '@line': %(lineno)s"
+        '{"message": "%(message)s", "level": "%(levelname)s", "name": "%(name)s", "line": "%(lineno)d", "time": "%(asctime)s"}'
     )
 )
 log.addHandler(custom_handler)
@@ -54,15 +66,15 @@ class logger(commands.Cog):
             The new logging level.
         """
         if level.upper == "DEBUG":
-            log.setLevel(logging.DEBUG)
+            logging.getLogger().setLevel(logging.DEBUG)
         if level.upper == "info":
-            log.setLevel(logging.INFO)
+            logging.getLogger().setLevel(logging.INFO)
         if level.upper == "WARNING":
-            log.setLevel(logging.WARNING)
+            logging.getLogger().setLevel(logging.WARNING)
         if level.upper == "ERROR":
-            log.setLevel(logging.ERROR)
+            logging.getLogger().setLevel(logging.ERROR)
         if level.upper == "CRITICAL":
-            log.setLevel(logging.CRITICAL)
+            logging.getLogger().setLevel(logging.CRITICAL)
 
 
 def setup(bot: commands.Bot) -> None:
