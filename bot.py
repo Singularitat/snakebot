e444eec4233d5bd7ef6a93d1d072671065f685a3
singularitat <singularbotnet@gmail.com>
2021-02-28 16:41:11 +1300

    Removed unused intents

--- bot.py
+++ bot.py
@@ -6,8 +6,14 @@ import config
 # Connects to discord, Sets command prefix, Removes default help command
 # Sets discord intents to enable certain gateway features that are necessary
 
+intents = discord.Intents.all()
+intents.invites = False
+intents.dm_typing = False
+intents.webhooks = False
+intents.integrations = False
+
 bot = commands.Bot(
-    intents=discord.Intents.all(),
+    intents=intents,
     command_prefix=commands.when_mentioned_or("."),
     case_insensitive=True,
     owner_ids=(225708387558490112, 204807103704072194, 198892706087436288),
@@ -17,7 +23,7 @@ bot = commands.Bot(
 if __name__ == "__main__":
     for extension in [
         f.name.replace(".py", "")
-        for f in os.scandir(f"{__file__[:-7]}/cogs")
+        for f in os.scandir("cogs")
         if f.is_file()
     ]:
         try:
