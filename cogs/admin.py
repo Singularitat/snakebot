1eeacec80f62f4c1d11aa5c2672276318d6095ea
singularitat <singularbotnet@gmail.com>
2021-03-02 15:08:55 +1300

    Formatting

--- cogs/admin.py
+++ cogs/admin.py
@@ -68,18 +68,18 @@ class admin(commands.Cog):
     async def say_permissions(self, ctx, member, channel):
         permissions = channel.permissions_for(member)
         e = discord.Embed(colour=member.colour)
-        avatar = member.avatar_url_as(static_format='png')
+        avatar = member.avatar_url_as(static_format="png")
         e.set_author(name=str(member), url=avatar)
         allowed, denied = [], []
         for name, value in permissions:
-            name = name.replace('_', ' ').replace('guild', 'server').title()
+            name = name.replace("_", " ").replace("guild", "server").title()
             if value:
                 allowed.append(name)
             else:
                 denied.append(name)
 
-        e.add_field(name='Allowed', value='\n'.join(allowed))
-        e.add_field(name='Denied', value='\n'.join(denied))
+        e.add_field(name="Allowed", value="\n".join(allowed))
+        e.add_field(name="Denied", value="\n".join(denied))
         await ctx.send(embed=e)
 
     @commands.command(hidden=True)
