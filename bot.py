b7a77415bfd9112d387c9decba1d4d7e2abf0dbc
singularitat <singularbotnet@gmail.com>
2021-03-25 21:57:57 +1300

    Switch to using leveldb

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import discord
 from discord.ext import commands
 import os
 import config
+import plyvel
+import pathlib
 
-# Connects to discord, Sets command prefix, Removes default help command
-# Sets discord intents to enable certain gateway features that are necessary
+
+db = plyvel.DB(f"{pathlib.Path(__file__).parent.absolute()}/db", create_if_missing=True)
+
 
 intents = discord.Intents.all()
 intents.invites = False
@@ -19,6 +22,8 @@ bot = commands.Bot(
     owner_ids=(225708387558490112, 204807103704072194, 198892706087436288),
     activity=discord.Game(name="Tax Evasion Simulator"),
 )
+
+bot.db = db
 
 if __name__ == "__main__":
     for extension in [
