344ecbdbe88c152379237701dd028b666e2bc043
singularitat <singularbotnet@gmail.com>
2021-03-26 15:45:06 +1300

    Remove menu buttons from pagination

--- cogs/utils/paginator.py
+++ cogs/utils/paginator.py
@@ -1,4 +1,3 @@
-import asyncio
 import discord
 from discord.ext.commands import Paginator as CommandPaginator
 from discord.ext import menus
@@ -15,62 +14,6 @@ class RoboPages(menus.MenuPages):
             else:
                 await self.message.delete()
         except discord.HTTPException:
-            pass
-
-    @menus.button("\N{INFORMATION SOURCE}\ufe0f", position=menus.Last(3))
-    async def show_help(self, payload):
-        """Shows this message."""
-        embed = discord.Embed(
-            title="Paginator help", description="Hello! Welcome to the help page."
-        )
-        messages = []
-        for (emoji, button) in self.buttons.items():
-            messages.append(f"{emoji}: {button.action.__doc__}")
-
-        embed.add_field(
-            name="What are these reactions for?",
-            value="\n".join(messages),
-            inline=False,
-        )
-        embed.set_footer(
-            text=f"We were on page {self.current_page + 1} before this message."
-        )
-        await self.message.edit(content=None, embed=embed)
-
-        async def go_back_to_current_page():
-            await asyncio.sleep(30.0)
-            await self.show_page(self.current_page)
-
-        self.bot.loop.create_task(go_back_to_current_page())
-
-    @menus.button("\N{INPUT SYMBOL FOR NUMBERS}", position=menus.Last(1.5))
-    async def numbered_page(self, payload):
-        """Lets you type a page number to go to."""
-        channel = self.message.channel
-        author_id = payload.user_id
-        to_delete = []
-        to_delete.append(await channel.send("What page do you want to go to?"))
-
-        def message_check(m):
-            return (
-                m.author.id == author_id
-                and channel == m.channel
-                and m.content.isdigit()
-            )
-
-        try:
-            msg = await self.bot.wait_for("message", check=message_check, timeout=30.0)
-        except asyncio.TimeoutError:
-            to_delete.append(await channel.send("Took too long."))
-            await asyncio.sleep(5)
-        else:
-            page = int(msg.content)
-            to_delete.append(msg)
-            await self.show_checked_page(page - 1)
-
-        try:
-            await channel.delete_messages(to_delete)
-        except AttributeError:
             pass
 
 
