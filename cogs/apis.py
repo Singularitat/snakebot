be165dde943283fcd7272ec60021c576b9621827
singularitat <singularbotnet@gmail.com>
2021-09-13 23:03:30 +1200

    Add another fact api that has a 50% chance of getting used

--- cogs/apis.py
+++ cogs/apis.py
@@ -135,14 +135,27 @@ class apis(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def fact(self, ctx):
-        """Gets a random fact."""
-        url = "https://uselessfacts.jsph.pl/random.json?language=en"
+    async def fact(self, ctx, index: int = None):
+        """Gets a random fact.
+
+        index: int
+            Can be either 0 or 1, mainly for testing purposes.
+        """
+        if index is None:
+            index = random.randint(0, 1)
+
+        url = (
+            "https://uselessfacts.jsph.pl/random.json?language=en",
+            "https://asli-fun-fact-api.herokuapp.com",
+        )[index]
 
         async with ctx.typing():
             data = await self.bot.get_json(url)
 
-            text = data["text"].replace("`", "`\u200b")
+            if index == 0:
+                text = data["text"].replace("`", "`\u200b")
+            else:
+                text = data["data"]["fact"]
 
             await ctx.send(
                 embed=discord.Embed(
