56760d3fca21fe8d3bca6bcda6a8b5d13f778cd8
Singularitat <singularbotnet@gmail.com>
2021-10-08 23:27:56 +1300

    Remove noise command

--- cogs/apis.py
+++ cogs/apis.py
@@ -129,32 +129,6 @@ class apis(commands.Cog):
         await ctx.send(data["file"])
 
     @commands.command()
-    async def noise(self, ctx, *, search):
-        """Uses everynoise.com to search spotify."""
-        url = f"https://everynoise.com/spotproxy.cgi?q={search}"
-        data = await self.bot.get_json(url)
-
-        output = ""
-        count = 0
-
-        for track in data["tracks"]["items"]:
-            if count == 5:
-                break
-            if track["type"] == "track":
-                output += track["external_urls"]["spotify"] + "\n"
-                count += 1
-
-        if output:
-            return await ctx.send(output)
-
-        await ctx.send(
-            embed=discord.Embed(
-                color=discord.Color.blurple(),
-                description="```Couldn't find any results```",
-            )
-        )
-
-    @commands.command()
     async def insult(self, ctx):
         """Insults you."""
         url = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
