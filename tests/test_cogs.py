fb5fc69dc35b510f03cd9884d7cde0b2fb640a12
Singularitat <singularbotnet@gmail.com>
2021-10-11 22:06:57 +1300

    Add test for story command

--- tests/test_cogs.py
+++ tests/test_cogs.py
@@ -106,6 +106,14 @@ class ApisCogTests(unittest.IsolatedAsyncioTestCase):
 
             self.assertIs(context.reply.call_args.kwargs.get("embed"), None)
 
+    async def story_command(self):
+        context = helpers.MockContext()
+
+        with self.subTest(command="story"):
+            await self.cog.story(self.cog, context)
+
+            self.assertIs(context.reply.call_args.kwargs.get("embed"), None)
+
     async def dad_joke_command(self):
         context = helpers.MockContext()
 
