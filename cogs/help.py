ad69fae8b3fcf0cdd483cb82105d4fcf1fc1f090
Singularitat <singularbotnet@gmail.com>
2021-10-17 23:30:04 +1300

    Formatting

--- cogs/help.py
+++ cogs/help.py
@@ -120,33 +120,44 @@ class HelpMenu(ui.View, menus.MenuPages):
     async def _get_kwargs_from_page(self, page):
         """This method calls ListPageSource.format_page class"""
         value = await super()._get_kwargs_from_page(page)
-        if 'view' not in value:
-            value.update({'view': self})
+        if "view" not in value:
+            value.update({"view": self})
         return value
 
     async def interaction_check(self, interaction):
         """Only allow the author that invoke the command to be able to use the interaction"""
         return interaction.user == self.ctx.author
 
-    @ui.button(emoji='<:before_fast_check:754948796139569224>', style=discord.ButtonStyle.blurple)
+    @ui.button(
+        emoji="<:before_fast_check:754948796139569224>",
+        style=discord.ButtonStyle.blurple,
+    )
     async def first_page(self, button, interaction):
         await self.show_page(0)
 
-    @ui.button(emoji='<:before_check:754948796487565332>', style=discord.ButtonStyle.blurple)
+    @ui.button(
+        emoji="<:before_check:754948796487565332>", style=discord.ButtonStyle.blurple
+    )
     async def before_page(self, button, interaction):
         await self.show_checked_page(self.current_page - 1)
 
-    @ui.button(emoji='<:stop_check:754948796365930517>', style=discord.ButtonStyle.blurple)
+    @ui.button(
+        emoji="<:stop_check:754948796365930517>", style=discord.ButtonStyle.blurple
+    )
     async def stop_page(self, button, interaction):
         self.stop()
         if self.delete_message_after:
             await self.message.delete(delay=0)
 
-    @ui.button(emoji='<:next_check:754948796361736213>', style=discord.ButtonStyle.blurple)
+    @ui.button(
+        emoji="<:next_check:754948796361736213>", style=discord.ButtonStyle.blurple
+    )
     async def next_page(self, button, interaction):
         await self.show_checked_page(self.current_page + 1)
 
-    @ui.button(emoji='<:next_fast_check:754948796391227442>', style=discord.ButtonStyle.blurple)
+    @ui.button(
+        emoji="<:next_fast_check:754948796391227442>", style=discord.ButtonStyle.blurple
+    )
     async def last_page(self, button, interaction):
         await self.show_page(self._source.get_max_pages() - 1)
 
