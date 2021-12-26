2d424f44a554dfea4be957025d8b7f21e72c7d31
Singularitat <singularbotnet@gmail.com>
2021-12-26 23:23:08 +1300

    Add balls command

--- cogs/images.py
+++ cogs/images.py
@@ -278,6 +278,14 @@ class images(commands.Cog):
         """
         await self.jeyy(ctx, "sensitive", url)
 
+    @commands.command()
+    async def balls(self, ctx, url: str = None):
+        """Turns an image into balls that are dropped.
+
+        url: str
+        """
+        await self.jeyy(ctx, "balls", url)
+
 
 def setup(bot: commands.Bot) -> None:
     """Starts the image cog."""
