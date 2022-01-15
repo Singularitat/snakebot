026649d3656551754ba4e61e2fb145e609413a73
Singularitat <singularbotnet@gmail.com>
2022-01-15 22:05:14 +1300

    Add shoot command

--- cogs/images.py
+++ cogs/images.py
@@ -403,6 +403,14 @@ class images(commands.Cog):
         """
         await self.jeyy(ctx, "pattern", url)
 
+    @commands.command(aliases=["kill"])
+    async def shoot(self, ctx, url: str = None):
+        """Shoots someone.
+
+        url: str
+        """
+        await self.jeyy(ctx, "shoot", url)
+
     @commands.command()
     async def images(self, ctx):
         """Shows all the image manipulation commands."""
