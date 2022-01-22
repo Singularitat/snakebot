2c618511c38613996bd41c40418bd3b937099908
Singularitat <singularbotnet@gmail.com>
2022-01-22 22:53:00 +1300

    Update vaccine command as the health.govt.nz was changed

--- cogs/useful.py
+++ cogs/useful.py
@@ -266,23 +266,22 @@ class useful(commands.Cog):
         async with ctx.typing(), self.bot.client_session.get(url) as resp:
             soup = lxml.html.fromstring(await resp.text())
 
-        data = soup.xpath(".//td")[:16]
+        data = soup.xpath(".//td")
 
         description = (
             "```prolog\nEligible Population Vaccinated %:\n  First Dose: "
-            f"{data[2].text}\n  Second Dose: {data[8].text}\n\nCumulative Total:\n  "
-            f"First Dose: {data[4].text}\n  Second Dose: {data[7].text}\n  Third Primary:"
-            f" {data[13].text}\n  Boosters: {data[15].text}\n\nVaccinations Yesterday:\n"
-            f"  First Dose: {data[0].text}\n  Second Dose: {data[6].text}\n"
-            f"  Third Primary: {data[12].text}\n  Boosters: {data[14].text}\n```"
+            f"{data[186].text_content()}\n  Second Dose: {data[189].text_content()}\n\nCumulative Total:\n  "
+            f"First Dose: {data[1].text}\n  Second Dose: {data[4].text}\n  Third Primary:"
+            f" {data[7].text}\n  Boosters: {data[9].text}\n\nVaccinations Yesterday:\n"
+            f"  First Dose: {data[0].text}\n  Second Dose: {data[3].text}\n"
+            f"  Third Primary: {data[6].text}\n  Boosters: {data[8].text}\n```"
         )
 
         await ctx.send(
             embed=discord.Embed(color=discord.Color.blurple(), description=description)
             .set_footer(text="Vaccine data from health.govt.nz")
             .set_image(
-                url="https://www.health.govt.nz/sites/default/files/images/our-work/"
-                "diseases-conditions/covid19/vaccine-data/uptake_summary_40_0.png"
+                url="https://www.health.govt.nz" + soup.xpath(".//img")[1].attrib["src"]
             )
         )
 
