2b11926130083b4269499f621388872e58a8d97b
Singularitat <singularbotnet@gmail.com>
2022-02-15 19:53:03 +1300

    Add test for idea command

--- tests/test_cogs.py
+++ tests/test_cogs.py
@@ -83,6 +83,14 @@ class ApisCogTests(unittest.IsolatedAsyncioTestCase):
             *[getattr(self, name)() for name in dir(self) if name.endswith("command")]
         )
 
+    async def idea_command(self):
+        context = helpers.MockContext()
+
+        with self.subTest(command="idea"):
+            await self.cog.idea(self.cog, context)
+
+            self.assertIsNone(context.send.call_args.kwargs.get("embed"))
+
     async def domains_command(self):
         context = helpers.MockContext()
 
