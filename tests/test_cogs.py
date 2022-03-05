a16e77c87f76a2201d5d8113517ee9c39a74eee3
Singularitat <singularbotnet@gmail.com>
2022-03-05 23:50:21 +1300

    Add test for atom command

--- tests/test_cogs.py
+++ tests/test_cogs.py
@@ -1220,6 +1220,15 @@ class MiscCogTests(unittest.IsolatedAsyncioTestCase):
             context.send.call_args.kwargs["embed"].color.value, 10038562
         )
 
+    async def atom_command(self):
+        context = helpers.MockContext()
+
+        await self.cog.atom(self.cog, context, "O")
+
+        self.assertNotEqual(
+            context.send.call_args.kwargs["embed"].color.value, 10038562
+        )
+
     async def test_vec4_command(self):
         context = helpers.MockContext()
         tests = (
